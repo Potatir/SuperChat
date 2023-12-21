7ad4c6d441b611209f3a174133baada2e456beef
Cumcordia <makmimmakarov228666@gmail.com>
2023-12-21 10:06:23 +0600

    v1.2

--- clientpart/main.py
+++ clientpart/main.py
@@ -1,17 +1,9 @@
 from customtkinter import *
-from PIL import Image
 from reg import regwin
 
 win = CTk()
 win.geometry("600x480")
 win.resizable(False, False)
-
-email_icon_data = Image.open("email-icon.png")
-password_icon_data = Image.open("password-icon.png")
-
-
-email_icon = CTkImage(dark_image=email_icon_data, light_image=email_icon_data, size=(20,20))
-password_icon = CTkImage(dark_image=password_icon_data, light_image=password_icon_data, size=(17,17))
 
 CTkLabel(master=win, text="").pack(expand=True, side="left")
 
@@ -27,12 +19,12 @@ CTkLabel(master=frame, text="Добро пожаловать!", text_color="#601
 CTkLabel(master=frame, text="Войдите в свой аккаунт", text_color="#7E7E7E", anchor="w", justify="left",
          font=("Arial Bold", 12)).pack(anchor="w", padx=(25, 0))
 
-CTkLabel(master=frame, text="  Логин", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
-         image=email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
+CTkLabel(master=frame, text="Логин", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
+         compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
 CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000").pack(anchor="w", padx=(25, 0))
 
-CTkLabel(master=frame, text="  Пароль:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
-         image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+CTkLabel(master=frame, text="Пароль:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
+         compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
 CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000",
          show="*").pack(anchor="w", padx=(25, 0))
 
--- clientpart/reg.py
+++ clientpart/reg.py
@@ -1,16 +1,8 @@
 from customtkinter import *
-from PIL import Image
-
 def regwin():
     win = CTk()
     win.geometry("600x580")
     win.resizable(False, False)
-
-    email_icon_data = Image.open("email-icon.png")
-    password_icon_data = Image.open("password-icon.png")
-
-    email_icon = CTkImage(dark_image=email_icon_data, light_image=email_icon_data, size=(20, 20))
-    password_icon = CTkImage(dark_image=password_icon_data, light_image=password_icon_data, size=(17, 17))
 
     CTkLabel(master=win, text="").pack(expand=True, side="left")
 
@@ -27,29 +19,28 @@ def regwin():
              font=("Arial Bold", 12)).pack(anchor="w", padx=(25, 0))
 
     CTkLabel(master=frame, text="  Логин", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
-             image=email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
+             compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
     CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1,
              text_color="#000000").pack(anchor="w", padx=(25, 0))
 
     CTkLabel(master=frame, text="  Пароль:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
-             image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+             compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
     CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000",
              show="*").pack(anchor="w", padx=(25, 0))
 
     CTkLabel(master=frame, text="  Имя:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
-             image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+             compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
     CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1,
              text_color="#000000").pack(anchor="w", padx=(25, 0))
 
     CTkLabel(master=frame, text="  Фамилия:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14),
-             image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+             compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
     CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1,
              text_color="#000000").pack(anchor="w", padx=(25, 0))
 
     CTkButton(master=frame, text="Зарегистрироваться", fg_color="#601E88", hover_color="#E44982",
               font=("Arial Bold", 12), text_color="#ffffff", width=225).pack(anchor="w", pady=(40, 0), padx=(25, 0))
-
-
     win.mainloop()
+
 
 regwin()
